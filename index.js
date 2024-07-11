ace5bc5b2b7b211409b62b53a0108c9292f6d345
MimFSD <mimislam878@gmail.com>
2024-07-11 10:55:27 +0600

    Fix some responsive issue

--- index.js
+++ index.js
@@ -345,7 +345,6 @@ async function run() {
 
 
 
-        
         app.get("/reserve-report/:id", verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
